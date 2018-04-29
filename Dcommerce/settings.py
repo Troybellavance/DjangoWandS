7c0af2343efa366f079f532d9088a89499077138
Troy Bellavance <33408159+Troybellavance@users.noreply.github.com>
2018-04-29 19:10:03 -0400

    Add files via upload

--- Dcommerce/settings.py
+++ Dcommerce/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@&mjrv=idhzeog$id952o#a(da57o%y2px0*xn5ll3fqe%c^dh'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -118,3 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_dc"),
+    '/var/www/static/',
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
