d1e7f7af6697c8e841946962515af000688b8012
Troy Bellavance <33408159+Troybellavance@users.noreply.github.com>
2018-06-15 03:36:16 -0400

    Account updates

--- accounts/views.py
+++ accounts/views.py
@@ -1,32 +1,56 @@
 from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.utils.http import is_safe_url
 
-from .forms import LoginForm, RegistrationForm
+from .forms import LoginForm, RegistrationForm, GuestForm
+from .models import GuestEmail
 
-# Create your views here.
+
+def guest_registration_view(request):
+    form_class = GuestForm(request.POST or None)
+    context = {"form": form_class}
+    next_get = request.GET.get('next')
+    next_post = request.POST.get('next')
+    redirect_path = next_get or next_post or None
+    if form_class.is_valid():
+        email    = form_class.cleaned_data.get("email")
+        new_guest_email = GuestEmail.objects.create(email=email)
+        request.session['guest_email_id'] = new_guest_email.id
+        if is_safe_url(redirect_path, request.get_host()):
+            return redirect(redirect_path)
+        else:
+            return redirect("/registration/")
+    return redirect("/registration/")
 
 def login_page(request):
     form_class = LoginForm(request.POST or None)
     context = {"form": form_class}
-    #print(request.user.is_authenticated())
+    next_get = request.GET.get('next')
+    next_post = request.POST.get('next')
+    redirect_path = next_get or next_post
     if form_class.is_valid():
         print(form_class.cleaned_data)
         username = form_class.cleaned_data.get("username")
         password = form_class.cleaned_data.get("password")
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            #print(request.user.is_authenticated())
             login(request, user)
-            # Redirect to a success page.
-            #context['form'] = LoginForm()
-            return redirect("/login")
+            try:
+                del request.session['guest_email_id']
+            except:
+                pass
+            if is_safe_url(redirect_path, request.get_host()):
+                return redirect(redirect_path)
+            else:
+                return redirect("/")
         else:
-            # Return an 'invalid login' error message.
             print("User does not exist/User password incorrect.")
     return render(request, "accounts/login.html", context)
 
 User = get_user_model()
+
+
 def registration_page(request):
     form_class = RegistrationForm(request.POST or None)
     context = {"form": form_class}
