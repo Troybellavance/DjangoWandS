c2d65146f492bca196d9910cac1c8578f827593e
Troy Bellavance <33408159+Troybellavance@users.noreply.github.com>
2018-06-20 22:16:51 -0400

    Address reuse on checkout url

--- Dcommerce/urls.py
+++ Dcommerce/urls.py
@@ -18,16 +18,22 @@ from django.conf.urls.static import static
 
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.views.generic import TemplateView
 
-from accounts.views import login_page, registration_page
+from accounts.views import login_page, registration_page, guest_registration_view
+from addresses.views import checkout_address_create_view, checkout_address_reuse_view
 from .views import home_page, about_page, contact_page
 
 urlpatterns = [
     url(r'^$', home_page, name='home'),
     url(r'^about/$', about_page, name='about'),
     url(r'^contact/$', contact_page, name='contact'),
+    url(r'^checkout/address/create/$', checkout_address_create_view, name='checkout_address_create'),
+    url(r'^checkout/address/reuse/$', checkout_address_reuse_view, name='checkout_address_reuse'),
     url(r'^login/$', login_page, name='login'),
+    url(r'^registration/guest/$', guest_registration_view, name='guest_registration'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^cart/', include("carts.urls", namespace='cart')),
     url(r'^registration/$', registration_page, name='registration'),
     url(r'^bootstrap/$', TemplateView.as_view(template_name='bootstrap/example.html')),
