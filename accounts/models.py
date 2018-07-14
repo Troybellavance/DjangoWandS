469b4ec3d72f3fd4503a918331d26c8cf87273c0
Troy Bellavance <33408159+Troybellavance@users.noreply.github.com>
2018-07-14 11:59:48 -0400

    Updated user models and admin

--- accounts/models.py
+++ accounts/models.py
@@ -7,13 +7,15 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password=None, is_active=True, is_staff=False, is_admin=False):
+    def create_user(self, email, first_name=None, last_name=None, password=None, is_active=True, is_staff=False, is_admin=False):
         if not email:
             raise ValueError("Users require an email.")
         if not password:
             raise ValueError("Users require a password.")
         user_obj = self.model(
-            email = self.normalize_email(email)
+            email = self.normalize_email(email),
+            first_name = first_name,
+            last_name = last_name
         )
         user_obj.set_password(password)
         user_obj.staff = is_staff
@@ -22,17 +24,21 @@ class UserManager(BaseUserManager):
         user_obj.save(using=self._db)
         return user_obj
 
-    def create_staff(self, email, password=None):
+    def create_staff(self, email, first_name=None, last_name=None, password=None):
         user = self.create_user(
                email,
+               first_name = first_name,
+               last_name = last_name,
                password=password,
                is_staff=True
         )
         return user
 
-    def create_superuser(self, email, password=None):
+    def create_superuser(self, email,first_name=None, last_name=None, password=None):
         user = self.create_user(
                 email,
+                first_name = first_name,
+                last_name = last_name,
                 password=password,
                 is_staff=True,
                 is_admin=True
@@ -42,13 +48,13 @@ class UserManager(BaseUserManager):
 
 class CustomUser(AbstractBaseUser):
     #username    = models.CharField(max_length=35)
-    #first_name   = models.Charfield(max_length=75, blank=True, null=True)
-    #last_name   = models.Charfield(max_length=75, blank=True, null=True)
-    email       = models.EmailField(max_length=75, default='tuskysclub@tuskysclub.com', unique=True)
-    staff       = models.BooleanField(default=False)
-    admin       = models.BooleanField(default=False)
-    active      = models.BooleanField(default=True)
-    timestamp   = models.DateTimeField(auto_now_add=True)
+    first_name   = models.CharField(max_length=75, blank=True, null=True)
+    last_name    = models.CharField(max_length=75, blank=True, null=True)
+    email        = models.EmailField(max_length=75, default='tuskysclub@tuskysclub.com', unique=True)
+    staff        = models.BooleanField(default=False)
+    admin        = models.BooleanField(default=False)
+    active       = models.BooleanField(default=True)
+    timestamp    = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'email'
 
@@ -60,9 +66,13 @@ class CustomUser(AbstractBaseUser):
         return self.email
 
     def get_full_name(self):
+        if self.first_name and self.last_name:
+            return self.first_name and self.last_name
         return self.email
 
     def get_short_name(self):
+        if self.first_name:
+            return self.first_name
         return self.email
 
     def has_perm(self, perm, obj=None):
