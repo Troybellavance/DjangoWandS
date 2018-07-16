c166860aef226f7948cee153bf56d13bba40e8e8
Troy Bellavance <33408159+Troybellavance@users.noreply.github.com>
2018-07-16 16:04:34 -0400

    Session signals added

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,7 @@ from django.views.generic import CreateView, FormView
 
 from .forms import LoginForm, RegistrationForm, GuestForm
 from .models import GuestEmail
+from .signals import user_logged_in
 
 
 def guest_registration_view(request):
@@ -39,6 +40,7 @@ class LoginView(FormView):
         user = authenticate(request, username=email, password=password)
         if user is not None:
             login(request, user)
+            user_logged_in.send(user.__class__, instance=user, request=request)
             try:
                 del request.session['guest_email_id']
             except:
