2c74fea27653bb3cafc42677c4f02d439909ae79
Troy Bellavance <33408159+Troybellavance@users.noreply.github.com>
2018-07-19 15:05:11 -0400

    Stripe integration

--- billing/views.py
+++ billing/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 import stripe
@@ -5,6 +6,11 @@ stripe.api_key = "sk_test_lhe3uvwYErAh2ECdCfg6YD3C"
 STRIPE_PUBLIC_KEY = 'pk_test_E06fqQ0w0Yh0gNtiUmRjPg9o'
 
 def payment_method_view(request):
+    return render(request, 'billing/payment-method.html', {"publish_key": STRIPE_PUBLIC_KEY})
+
+
+def payment_method_createview(request):
     if request.method == "POST":
         print(request.POST)
-    return render(request, 'billing/payment-method.html', {"publish_key": STRIPE_PUBLIC_KEY})
+        return JsonResponse({"message": "Finished"})
+    return HttpResponse("error", status_code=401)
