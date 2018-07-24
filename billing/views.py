7fa1fe4b810f1c71a19acda302e08db0bb3a9531
Troy Bellavance <33408159+Troybellavance@users.noreply.github.com>
2018-07-23 23:17:08 -0400

    Integrating payment into backend.

--- billing/views.py
+++ billing/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
 
 import stripe
@@ -8,6 +8,12 @@ stripe.api_key = "sk_test_lhe3uvwYErAh2ECdCfg6YD3C"
 STRIPE_PUBLIC_KEY = 'pk_test_E06fqQ0w0Yh0gNtiUmRjPg9o'
 
 def payment_method_view(request):
+    if request.user.is_authenticated():
+        billing_profile = request.user.billingprofile
+        customer_id = billing_profile.customer_id
+    billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+    if not billing_profile:
+        return redirect("/cart")
     next_url = None
     next_get = request.GET.get('next')
     if is_safe_url(next_get, request.get_host()):
@@ -17,6 +23,8 @@ def payment_method_view(request):
 
 def payment_method_createview(request):
     if request.method == "POST" and request.is_ajax():
-        print(request.POST)
+        billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+        if not billing_profile:
+            return HttpResponse("message": "Cannot find this user.", status_code=401)
         return JsonResponse({"message": "Your card was successfully added."})
     return HttpResponse("error", status_code=401)
