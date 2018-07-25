30e94a4c90dd446f1f8edfa955ee4d5e06ca853d
Troy Bellavance <33408159+Troybellavance@users.noreply.github.com>
2018-07-25 16:07:49 -0400

    User registration error fixed.

--- accounts/forms.py
+++ accounts/forms.py
@@ -28,40 +28,13 @@ class RegistrationForm(forms.ModelForm):
         return password2
 
     def save(self, commit=True):
-        user = super(UserAdminCreationForm, self).save(commit=False)
+        user = super(RegistrationForm, self).save(commit=False)
         user.set_password(self.cleaned_data["password1"])
         # user.active = False
         if commit:
             user.save()
         return user
 
-# class RegistrationForm(forms.Form):
-#     username = forms.CharField()
-#     email = forms.EmailField()
-#     password = forms.CharField(widget=forms.PasswordInput)
-#     password2 = forms.CharField(label= 'Confirm password.', widget=forms.PasswordInput)
-#
-#     def clean_username(self):
-#         username = self.cleaned_data.get('username')
-#         qs = User.objects.filter(username=username)
-#         if qs.exists():
-#             raise forms.ValidationError("Username is already taken.")
-#         return username
-#
-#     def clean_email(self):
-#         email = self.cleaned_data.get('email')
-#         qs = User.objects.filter(email=email)
-#         if qs.exists():
-#             raise forms.ValidationError("Email is already taken.")
-#         return email
-#
-#     def clean(self):
-#         data = self.cleaned_data
-#         password = self.cleaned_data.get('password')
-#         password2 = self.cleaned_data.get('password2')
-#         if password2 != password:
-#             raise forms.ValidationError("Passwords must match.")
-#         return data
 
 class UserAdminCreationForm(forms.ModelForm):
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
