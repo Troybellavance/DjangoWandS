9890d1e9a095647168265f17364a96a48a503144
Troy Bellavance <33408159+Troybellavance@users.noreply.github.com>
2018-08-12 11:42:35 -0400

    Clean emailing base class

--- emailing/utilities.py
+++ emailing/utilities.py
@@ -1,3 +1,4 @@
+import json
 import requests
 from django.conf import settings
 
@@ -14,18 +15,31 @@ class Mailchimp_emailing(object):
         self.key = MAILCHIMP_API_KEY
         self.api_url = "https://{dc}.api.mailchimp.com/3.0".format(
                      dc=MAILCHIMP_DATA_CENTER
-                     )
+        )
         self.list_id = MAILCHIMP_EMAIL_LIST_ID
+        self.list_endpoint = '{api_url}/lists/{list_id}'.format(
+                               api_url = self.api_url
+                               list_id=self.list_id
+        )
 
     def check_if_subscribed(self, email):
         endpoint = self.api_url
-        request = requests.get(endpoint, auth=("", self.key))
-        return request.json()
+        req = requests.get(endpoint, auth=("", self.key))
+        return req.json()
+
+    def valid_status_check(self, status):
+        options = ['subscribed','unsubscribed','cleaned','pending']
+        if status not in choices:
+            raise ValueError("Not a valid status for emails")
+        return status
 
     def add_email_subscription(self, email):
+        status = "subscribed"
+        self.valid_status_check(status)
         data = {
-             "email": email
+             "email_address": email,
+             "status": status
         }
-        endpoint = self.api_url
-        request = requests.post(endpoint, auth=("", self.key), data=data)
-        return request.json()
+        endpoint = self.list_endpoint + "/members"
+        req = requests.post(endpoint, auth=("", self.key), data=json.dumps(data))
+        return req.json()
