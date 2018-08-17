2db775e793792cc43bbf0df5d2088c96c10542e5
Troy Bellavance <33408159+Troybellavance@users.noreply.github.com>
2018-08-17 01:34:37 -0400

    Formatting and API fix.

--- emailing/utilities.py
+++ emailing/utilities.py
@@ -9,25 +9,20 @@ MAILCHIMP_EMAIL_LIST_ID = getattr(settings, "MAILCHIMP_EMAIL_LIST_ID", None)
 
 
 
-class Mailchimp_emailing(object):
+class MailchimpEmailing(object):
     def __init__(self):
-        super(Mailchimp_emailing, self).__init__()
+        super(MailchimpEmailing, self).__init__()
         self.key = MAILCHIMP_API_KEY
-        self.api_url = "https://{dc}.api.mailchimp.com/3.0".format(
-                     dc=MAILCHIMP_DATA_CENTER
-        )
+        self.api_url = "https://{dc}.api.mailchimp.com/3.0".format(dc=MAILCHIMP_DATA_CENTER)
         self.list_id = MAILCHIMP_EMAIL_LIST_ID
-        self.list_endpoint = '{api_url}/lists/{list_id}'.format(
-                               api_url = self.api_url,
-                               list_id=self.list_id
-        )
+        self.list_endpoint = '{api_url}/lists/{list_id}'.format(api_url=self.api_url, list_id=self.list_id)
 
     def check_if_subscribed(self, email):
         endpoint = self.api_url
         req = requests.get(endpoint, auth=("", self.key))
         return req.json()
 
-    def valid_status_check(self, status):
+    def check_validity_status(self, status):
         options = ['subscribed','unsubscribed','cleaned','pending']
         if status not in options:
             raise ValueError("Not a valid status for emails")
@@ -35,7 +30,7 @@ class Mailchimp_emailing(object):
 
     def add_email_subscription(self, email):
         status = "subscribed"
-        self.valid_status_check(status)
+        self.check_validity_status(status)
         data = {
              "email_address": email,
              "status": status
