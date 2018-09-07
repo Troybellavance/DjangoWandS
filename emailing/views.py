b673db9b5e415a762bf28ba2afcdcb3c53627305
Troy Bellavance <33408159+Troybellavance@users.noreply.github.com>
2018-09-06 23:59:57 -0400

    Emailing login

--- emailing/views.py
+++ emailing/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.contrib.messages.views import SuccessMessageMixin
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.views.generic import UpdateView
 
 from .forms import EmailingPreferencesForm
 from .models import EmailingPreferences
 
-class EmailingPreferencesUpdateView(UpdateView):
+class EmailingPreferencesUpdateView(SuccessMessageMixin, UpdateView):
     form_class = EmailingPreferencesForm
     template_name = 'base/forms.html'
     success_url = '/settings/email/'
+    success_message = 'Your email preferences were updated.'
+
+    def dispatch(self, *args, **kwargs):
+        user = self.request.user
+        if not user.is_authenticated():
+            return redirect("/login/?next=/settings/email/")
+        return super(EmailingPreferencesUpdateView, self).dispatch(*args, **kwargs)
 
     def get_context_data(self, *args, **kwargs):
         context = super(EmailingPreferencesUpdateView, self).get_context_data(*args, **kwargs)
